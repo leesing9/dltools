7552fa6b2622df212f2a7c000fedf0b10ee33f24
MinKyu Kim <qlwlal@naver.com>
2021-02-24 16:36:24 +0900

    머지 전 커밋

--- dltools/dataset/temperature_RGB.py
+++ dltools/dataset/temperature_RGB.py
@@ -1,31 +1,39 @@
-import numpy as np
+import numpy
 import pandas
 from pathlib import Path
-from PIL import Image
 
-class RGB:
-    def __init__(self, r, g, b) -> None:
-        self.key = f'{r},{g},{b}'
-        self.val = [r,g,b]
+dictionary = {0 : [0, 0, 0],
+              1 : [1, 1, 1],
+              2: [2, 2, 2],
+              3 : [3, 3, 3],
+              4 : [4, 4, 4],
+              5 : [5, 5, 5],
+              6 : [6, 6, 6],
+              7 : [7, 7, 7],
+              8 : [8, 8, 8],
+              9 : [9, 9, 9],
+              10 : [10, 10, 10],
+              11 : [11, 11, 11],
+              12 : [12, 12, 12],
+              13 : [13, 13, 13],
+              14 : [14, 14, 14],
+              15 : [15, 15, 15],
+              16 : [16, 16, 16],
+              17 : [17, 17, 17],
+              18 : [18, 18, 18],
+              19 : [19, 19, 19],
+              20 : [20, 20, 20],
+              21 : [21, 21, 21],
+              22 : [22, 22, 22],
+              23 : [23, 23, 23],
+              24 : [24, 24, 24],
+              25 : [25, 25, 25],
+              26 : [26, 26, 26],
+              27 : [27, 27, 27],
+}
+image = numpy.ones(shape = (28, 28, 3))
 
-    def __repr__(self) -> str:
-        return self.key
-
-    def __iter__(self, *args, **kwds):
-        return self.val
-        
-thermal_dic = {}
-t = 0
-for r in range(256):
-    for b in range(256):
-        for g in range(256):
-            thermal_dic[RGB(r,g,b)] = t
-            t+=1
-
-def thermal_matching(image, thermal_dic, outidr, filename):
-    for key, val in thermal_dic:
-        np.where(image==val,np.tile(key,3), image)
-    image.re
+def thermal_matching(image, dictionary, outidr, filename):
     for rowIndex in range(image.shape[0]):
 
         for columnIndex in range(image.shape[1]):
@@ -43,18 +51,9 @@ def thermal_matching(image, thermal_dic, outidr, filename):
 
             # rgb = image[rowIndex, columnIndex, : ]
 
-            key = [key for key, value in thermal_dic.items() if value == image[rowIndex, columnIndex, : ].tolist()]
+            key = [key for key, value in dictionary.items() if value == image[rowIndex, columnIndex, : ].tolist()]
             temperature[rowIndex, columnIndex] = key[0]
 
     dataFrame = pandas.DataFrame(temperature)
 
     dataFrame.to_csv(str(Path(outidr)/f'{filename}.csv'), header = False, index = False)
-
-
-if __name__ == '__main__':
-    path = Path('d:\Capture/P0510-0515_D50_AD30_R0-T26.AVI_20210224_152217.936.png')
-    outidr = 'd:/'
-    filename = path.stem
-    img = Image.open(path)
-    thermal_matching(img, thermal_dic,outidr, filename)
-    
